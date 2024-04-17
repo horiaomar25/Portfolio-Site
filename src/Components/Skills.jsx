5db32feec4972ed82f4b6c22597aa78eb0fe1a0d
horiaomar25 <horiaomar6@gmail.com>
2024-04-17 20:13:58 +0100

    added: update

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -20,7 +20,7 @@ const Skills = () => {
   // Render each technology dynamically
   const renderTechnologies = () => {
     return technologies.map((tech, index) => (
-      <div key={index} className="mb-4 md:mb-0 md:w-1/4 flex flex-col items-center w-full">
+      <div key={index} className="mb-4 md:mb-0 md:w-1/2 lg:w-1/3 flex flex-col items-center w-full">
         <p className="text-lg font-bold">{tech.name}</p>
         <div className="flex items-center">
           <span className="text--500 mr-1">&#10003;</span>
@@ -33,9 +33,9 @@ const Skills = () => {
   return (
     <>
       <h2 className="text-center text-4xl overflow-hidden">Tech Stack</h2>
-      <section className="flex  justify-center item-center border border-black m-8 rounded-lg" id="skills">
-        <div className="w-full flex justify-center items-center md:w-1/2 lg:w-1/2 xl:w-2/3 px-4 mt-8">
-          <div className="grid grid-cols-1 w-full md:grid-cols-1 lg:grid-cols-3 gap-4">
+      <section className="flex justify-center items-center border border-black m-8 rounded-lg" id="skills">
+        <div className="w-full flex justify-center items-center md:flex-col lg:w-2/3 p-6">
+          <div className="flex flex-wrap justify-center">
             {renderTechnologies()}
           </div>
         </div>
@@ -46,3 +46,5 @@ const Skills = () => {
 
 export default Skills;
 
+
+  
